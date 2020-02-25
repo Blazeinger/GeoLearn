5f5c6cf915fce24aff1d7267c94a85b4412dcde5
JoshusTenakhongva <jtt75@nau.edu>
2020-02-24 17:12:25 -0700

    added google images scraping functionality

--- testsite/biodiversity/imageObtainer.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import requests
-from io import BytesIO
-from StringIO import StringIO
-
-response = requests.get( "https://en.wikipedia.org/wiki/Montane_vole" )
-picture = StringIO( response.content )
-img = Image.open( picture )
