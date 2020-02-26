f44fdccf06e748f660e4a41d5d62224013c674a7
SPrasse <prassesamuel@gmail.com>
2020-02-25 22:54:11 -0700

    Add files via upload

--- GeoLearn App/python_scripts/urls.py
+++ GeoLearn App/python_scripts/urls.py
@@ -1,17 +1,13 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
-'''
-	The things in this list are urls that are accessible on the website
-	Each url is associated with a different function in the 'views' python script 
-	Each of these urls is given a name. These names are what are referenced in the html
-	'''
-	
-urlpatterns = [ 
-	path( '', views.index, name='index' ), 
-	path( 'brother/', views.brother, name='brother' ), 
-	path( 'Slides.html/', views.slides, name='slides' ), 
+urlpatterns = [
+	# index is the name of the function in views.py
+	path( '', views.index, name='index' ),
+	path( 'brother/', views.brother, name='brother' ),
+	path( 'Slides.html/', views.slides, name='slides' ),
 	path( 'index.html/', views.index, name='index' ),
 	path( 'bio_output/', views.biodiversity_submit, name='bio_submit' ),
-	path( 'climate_output/', views.climate_submit, name='climate_submit' ), 
-] 
+	url(r'climate_output', views.climate_submit, name="climate_submit")
+]
--- GeoLearn App/python_scripts/views.py
+++ GeoLearn App/python_scripts/views.py
@@ -1,54 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Make sure you import the functions you want to use from other python scripts 
 from .biodiversity.biodiversity_script_geolearn import find_animals_script
-from .biodiversity.biodiversity_image_scraper import image_scraper
-from .climate_change.time_lapse import time_lapse
+#from .climate_change.time_lapse import time_lapse
+from subprocess import run,PIPE
+import sys
 
 # Create your views here.
-'''
-Views are the python function that are associated with different urls. 
-When you go to a URL in the 'urls.py', it will run the function in this script 
-
-In the urls script, you can see that the path '' is associated with the index
-function just below. When you go to localhost with no extension, it will run
-the index function. 
-
-If you go to localhost/brother, it will run the brother function. 
-'''
 def index( request ):
-
-	# render, the way I'm using it, just runs an html file. 
-	# It may do other things that I don't know, but that's what it's doing here
 	return render( request, 'index.html' )
-	
+
 def brother( request ):
 	return HttpResponse( "hell yeah, brother" )
-	
+
 def slides( request ):
 	return render( request, 'Slides.html' )
 
-
-# When this function is run, it will fun the 'find_animals_script' function 
-# Then it renders the 'Slides.html' file, giving it the argument "biodiversity" 
 def biodiversity_submit( request ):
-	'''
-	# For now, these don't matter. I do want to keep them here for future reference just in case
 	if request.method == 'POST':
-		output = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' 
+		output = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
 	else:
-		output = 'Biodiversity Slideshow submission' 
+		output = 'Biodiversity Slideshow submission'
 	#arguments = request.POST.get( 'param' )
 	#print( arguments )
-	#image_scraper( 'fennec fox' )
-	'''
-	output = "biodiversity"
 	find_animals_script( 35, -111 )
+	#output = arguments
 	return render( request, 'Slides.html', {'message': output} )
-	
+
 def climate_submit( request ):
-	
-	time_lapse()
-	output = "cliamte change script ran successfully"
-	return render( request, 'Slides.html', {'message': output} )
+	lat = request.POST.get('lat')
+	lng = request.POST.get('long')
+
+	out = run([sys.executable,
+	'//mnt//c//Users//Samuel Prasse//Documents//GitHub//GeoLearn//GeoLearn App//GeoLearn-django_website//testsite//polls//climate_change//time_lapse.py', lat, lng], shell=False, stdout=PIPE)
+
+	#time_lapse(lat, lng)
+	output = "climate change script run successfully"
+
+	return render( request, 'Slides.html', {'message': out.stdout} )
