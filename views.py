c7e474fc6d6d956f49abadefc5bcce0fdfa1bb62
SPrasse <prassesamuel@gmail.com>
2020-02-27 09:50:35 -0700

    Fixed Comments

---
+++ views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .biodiversity.biodiversity_script_geolearn import find_animals_script
+from .biodiversity.biodiversity_image_scraper import image_scraper
+#from .climate_change.time_lapse import time_lapse
+from subprocess import run,PIPE
+import sys
+
+# Create your views here.
+'''
+Views are the python function that are associated with different urls.
+When you go to a URL in the 'urls.py', it will run the function in this script
+In the urls script, you can see that the path '' is associated with the index
+function just below. When you go to localhost with no extension, it will run
+the index function.
+If you go to localhost/brother, it will run the brother function.
+'''
+def index( request ):
+	# render, the way I'm using it, just runs an html file.
+	# It may do other things that I don't know, but that's what it's doing here
+	return render( request, 'index.html' )
+
+def brother( request ):
+	return HttpResponse( "hell yeah, brother" )
+
+def slides( request ):
+	return render( request, 'Slides.html' )
+
+# When this function is run, it will fun the 'find_animals_script' function
+# Then it renders the 'Slides.html' file, giving it the argument "biodiversity"
+def biodiversity_submit( request ):
+	'''
+	# For now, these don't matter. I do want to keep them here for future reference just in case
+	if request.method == 'POST':
+		output = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+	else:
+		output = 'Biodiversity Slideshow submission'
+	#arguments = request.POST.get( 'param' )
+	#print( arguments )
+	#image_scraper( 'fennec fox' )
+	'''
+	output = "biodiversity"
+	find_animals_script( 35, -111 )
+	return render( request, 'Slides.html', {'message': output} )
+
+def climate_submit( request ):
+	lat = request.POST.get('lat')
+	lng = request.POST.get('long')
+
+	out = run([sys.executable,
+	'//mnt//c//Users//Samuel Prasse//Documents//GitHub//GeoLearn//GeoLearn App//GeoLearn-django_website//testsite//polls//climate_change//time_lapse.py', lat, lng], shell=False, stdout=PIPE)
+
+	#time_lapse(lat, lng)
+	output = "climate change script run successfully"
+
+	return render( request, 'Slides.html', {'message': out.stdout} )
