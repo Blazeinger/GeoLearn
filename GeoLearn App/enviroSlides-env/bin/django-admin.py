9094b6e529f6e7921cb630f4b6b977a961f678cb
SPrasse <prassesamuel@gmail.com>
2020-04-16 14:52:52 -0700

    Beanstalk info

---
+++ GeoLearn App/enviroSlides-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/samuel/Documents/Capstone/GeoLearn/GeoLearn App/enviroSlides-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
